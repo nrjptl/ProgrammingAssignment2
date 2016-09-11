258761289bc309d7147e07e6c16cd0f6e7b5201a
nrjptl <nirajpatel2992@gmail.com>
2016-09-11 20:00:03 +0530

    Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,12 +2,15 @@
 
 
 ## makeCacheMatrix make a list that holds matrix and it's inverse
+## It takes a matrix as an argument and store it into the list,
+## along with inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 	mat_inverse <- matrix()
     mat_inverse <- NULL
     set <- function(y) {
         x <<- y
+		## set mat_inverse = NULL when argument matrix is changes.
         mat_inverse <<- NULL
     }
     get <- function() x
@@ -26,11 +29,13 @@ cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of matrix returned by 'x$get()'
 	mat_inverse <- x$get_mat_inverse()
     if(!is.null(mat_inverse)) {
+		## Getting cached mat_inverse from the enviroment.
         message("getting cached data")
         return(mat_inverse)
     }
-    data <- x$get()
-    mat_inverse <- solve(data)
+	
+	## Calculate the inverse of the matrix when mat_inverse = NULL
+    mat_inverse <- solve(x$get())
     x$set_mat_inverse(mat_inverse)
     mat_inverse
 }
